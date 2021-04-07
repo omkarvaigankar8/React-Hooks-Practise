7352a8044fe6ffe884768297808bd811df735122
Omkar Vaigankar <omkarvaigankar8@gmail.com>
2021-04-07 15:35:34 +0530

    added useContext

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,105 +1,124 @@
-import React, { useState, useEffect, useCallback } from 'react';
-// import axios from 'axios';
+import React, { useReducer, useEffect, useCallback } from 'react';
+
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
-const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('Should not get there!');
+  }
+};
 
-  const [error, setError]= useState(null);
-  // useEffect(() => {
-  //   fetch('https://hooks-f79e5-default-rtdb.firebaseio.com/ingredients.json')
-  //   .then(response => response.json()
-  //   ).then(responseData => {
-  //     const loadedIngredients=[];
-  //     for(const key in responseData) {
-  //       loadedIngredients.push({
-  //         id:key,
-  //         title:responseData[key].title,
-  //         amount: responseData[key].amount
-  //       });
-  //     }
-  //   setUserIngredients(loadedIngredients);
-  //   });
-  // },[]);//acts like componentDidMount with an empty array
+const httpReducer = (curHttpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return { loading: true, error: null };
+    case 'RESPONSE':
+      return { ...curHttpState, loading: false };
+    case 'ERROR':
+      return { loading: false, error: action.errorMessage };
+    case 'CLEAR':
+      return { ...curHttpState, error: null };
+    default:
+      throw new Error('Should not be reached!');
+  }
+};
+
+const Ingredients = () => {
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {
+    loading: false,
+    error: null
+  });
+  // const [userIngredients, setUserIngredients] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
 
   useEffect(() => {
     console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients)
-  },[]);
+    // setUserIngredients(filteredIngredients);
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
+  }, []);
 
   const addIngredientHandler = ingredient => {
-    setIsLoading(true);
-    fetch('https://hooks-f79e5-default-rtdb.firebaseio.com/ingredients.json',{
-      method:'POST',
-      body:JSON.stringify(ingredient),
-      headers:{ 'Content-Type' : 'application/json'}
-    }).then(response =>{
-      setIsLoading(false);
-      return response.json();
-    }).then(responseData =>{
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients, 
-        { id : responseData.name, ...ingredient}
-      ]);
-    }).catch(error => {
-      setError("Something went wrong");
-    });
-  }
+    dispatchHttp({ type: 'SEND' });
+    fetch('https://hooks-f79e5-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => {
+        dispatchHttp({ type: 'RESPONSE' });
+        return response.json();
+      })
+      .then(responseData => {
+        // setUserIngredients(prevIngredients => [
+        //   ...prevIngredients,
+        //   { id: responseData.name, ...ingredient }
+        // ]);
+        dispatch({
+          type: 'ADD',
+          ingredient: { id: responseData.name, ...ingredient }
+        });
+      });
+  };
 
-  // const addIngredientHandler = ingredient => {
-    
-  //   const userIngredient = setUserIngredients(prevIngredients => [
-  //     ...prevIngredients,
-  //     {id :Math.random().toString(), ...ingredient}
-  //   ]);
+  const removeIngredientHandler = ingredientId => {
+    dispatchHttp({ type: 'SEND' });
+    fetch(
+      `https://hooks-f79e5-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+      {
+        method: 'DELETE'
+      }
+    )
+      .then(response => {
+        dispatchHttp({ type: 'RESPONSE' });
+        // setUserIngredients(prevIngredients =>
+        //   prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+        // );
+        dispatch({ type: 'DELETE', id: ingredientId });
+      })
+      .catch(error => {
+        dispatchHttp({ type: 'ERROR', errorMessage: 'Something went wrong!' });
+      });
+  };
 
-  //   axios.post('https://hooks-f79e5-default-rtdb.firebaseio.com/ingredients.json',userIngredient)
-  //   .then(response => {
-  //     console.log(response);
-  //   })
-  //   .catch(error =>{
-  //     console.log(error);
-  //   });
-  // }
+  const clearError = () => {
+    dispatchHttp({ type: 'CLEAR' });
+  };
 
-
-  const removeIngredientHandler = (ingredientId) => {
-    setIsLoading(true);
-    fetch(`https://hooks-f79e5-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,{
-      method:'DELETE'
-    }).then(response => {
-      setIsLoading(true)
-      setUserIngredients(prevIngredients =>
-      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-      );
-    }).catch(error => {
-      setError("Something went wrong");
-      isLoading(false);
-    });
-  }
-
-  const clearError =() => {
-    setError(null);
-  }
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError} >{error}</ErrorModal>}
-      <IngredientForm  onAddIngredient ={addIngredientHandler} loading={isLoading}/>
+      {httpState.error && (
+        <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>
+      )}
+
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={httpState.loading}
+      />
 
       <section>
-        <Search  onLoadIngredients ={filteredIngredientsHandler}/>
-          <IngredientList ingredients = {userIngredients}
-          onRemoveItem= {removeIngredientHandler} />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
